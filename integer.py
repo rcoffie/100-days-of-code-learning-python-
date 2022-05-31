17b00d05e546824fcfc6429c08ec2b9d02ccccab
Robert <rcoffie22@yahoo.com>
2022-05-31 16:40:03 +0000

    day1 integer

---
+++ integer.py
@@ -0,0 +1,56 @@
+# Artithmetic Operators 
+# Addition: 3+2 
+print(3+2)
+# Subraction: 3-2 
+print(3-2)
+# Multiplication: 3*2 
+print(3*2)
+# Division: 3/2 
+print(3/2)
+# Floor Division: 3//2 
+print(3//2)
+# Exponent: 3**2 
+print(3**2)
+# Modulus:  3%2 
+print(3%2)
+
+
+#increament 
+
+num = 1 
+num = num + 1 
+num += 1 
+print(num)
+
+#using round 
+ #round() function returns a floating point number that is a rounded version of the specified number
+print(round(3.75))
+print(round(3.1))
+
+#Comparisons:
+#Equal: 3==2 
+#Not Equal: 3!=2 
+#Greater Than: 3>2 
+#Less Than: 3<2 
+#Greater or Equal: 3>=2 
+#Less or Equal: 3<=2 
+
+num_1 = 3 
+num_2 = 2 
+
+#note if the below statement is True is going to return True if False is going to Return False 
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+#casting string to integers 
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
